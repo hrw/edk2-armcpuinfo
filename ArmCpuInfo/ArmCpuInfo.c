36b7a97748829b1d613afab9077472a200add9e4
Marcin Juszkiewicz <marcin.juszkiewicz@linaro.org>
2023-12-01 18:37:02 +0100

    bump to v1.1.1

--- ArmCpuInfo/ArmCpuInfo.c
+++ ArmCpuInfo/ArmCpuInfo.c
@@ -2928,7 +2928,7 @@ UefiMain (
   Aa64Smfr0 = ArmReadIdAA64Smfr0 ();
   Aa64Zfr0  = ArmReadIdAA64Zfr0 ();
 
-  AsciiPrint ("ArmCpuInfo v1.1.0\n");
+  AsciiPrint ("ArmCpuInfo v1.1.1\n");
   AsciiPrint ("\n");
 
   AsciiPrint ("ID_AA64MMFR0_EL1 = 0x%016lx\n", Aa64Mmfr0);
