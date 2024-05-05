e1e1bd763687a28743a51df7330e9ae7e0c07e92
Marcin Juszkiewicz <marcin@juszkiewicz.com.pl>
2024-05-05 15:43:03 +0200

    start handling ID_AA64DFR1
    So far only read and show value.
    
    bug #5

--- ArmCpuInfo/ArmCpuInfo.c
+++ ArmCpuInfo/ArmCpuInfo.c
@@ -3462,6 +3462,7 @@ UefiMain (
   UINT64  Aa64Afr0;
   UINT64  Aa64Afr1;
   UINT64  Aa64Dfr0;
+  UINT64  Aa64Dfr1;
   UINT64  Aa64Isar0;
   UINT64  Aa64Isar1;
   UINT64  Aa64Isar2;
@@ -3476,6 +3477,7 @@ UefiMain (
   Aa64Afr0  = ArmReadIdAA64Afr0 ();
   Aa64Afr1  = ArmReadIdAA64Afr1 ();
   Aa64Dfr0  = ArmReadIdAA64Dfr0 ();
+  Aa64Dfr1  = ArmReadIdAA64Dfr1 ();
   Aa64Isar0 = ArmReadIdAA64Isar0 ();
   Aa64Isar1 = ArmReadIdAA64Isar1 ();
   Aa64Isar2 = ArmReadIdAA64Isar2 ();
@@ -3492,6 +3494,7 @@ UefiMain (
 
   AsciiPrint ("ID_AA64AFR0_EL1  = 0x%016lx\n", Aa64Afr0);
   AsciiPrint ("ID_AA64AFR1_EL1  = 0x%016lx\n", Aa64Afr1);
+  AsciiPrint ("ID_AA64DFR1_EL1  = 0x%016lx\n", Aa64Dfr1);
   AsciiPrint ("ID_AA64MMFR0_EL1 = 0x%016lx\n", Aa64Mmfr0);
   AsciiPrint ("ID_AA64MMFR1_EL1 = 0x%016lx\n", Aa64Mmfr1);
   AsciiPrint ("ID_AA64MMFR2_EL1 = 0x%016lx\n", Aa64Mmfr2);
